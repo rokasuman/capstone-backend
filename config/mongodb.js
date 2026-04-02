1902bf811c8e5af6446716970b7d2000b0f9c9ee
rokasuman <roka7362@gmail.com>
2026-04-02 12:19:00 +1100

    fix mongdb url

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,9 +1,20 @@
 import mongoose from "mongoose";
 
-const connectDB = async ()=>{
-    mongoose.connection.on("connected",()=>console.log("Database is Connected"))
-    console.log(process.env.MONGODB_URL)
-    await mongoose.connect(`${process.env.MONGODB_URL}/NovaHealth`)
+const connectDB = async () => {
+  try {
+    console.log("Mongo URL:", process.env.MONGODB_URL);
 
-}
-export default connectDB
+    if (!process.env.MONGODB_URL) {
+      throw new Error("MONGODB_URL is not defined in environment variables");
+    }
+
+    await mongoose.connect(process.env.MONGODB_URL);
+
+    console.log("Database is Connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
