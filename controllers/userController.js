739281a30193a8ccb00a0adeabbea15ba6605c09
rokasuman <roka7362@gmail.com>
2026-04-09 19:23:54 +1000

    checking the email

--- controllers/userController.js
+++ controllers/userController.js
@@ -214,6 +214,7 @@ const bookAppointment = async (req, res) => {
 
     const newAppointment = new appointmentModel(appointmentData);
     await newAppointment.save();
+    console.log("email:",userData.email)
     sendAppointmentEmail(
       userData.email,
       userData.name,
