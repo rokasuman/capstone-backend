06437a3103a1073b20c15af910a25ccc8b81c889
rokasuman <roka7362@gmail.com>
2026-04-09 14:27:36 +1000

    fix the email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer"
 
 //creating the transger
 
-const transfer = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
     service:"gmail",
     auth:{
         user:"process.env.EMAIL_USER",
