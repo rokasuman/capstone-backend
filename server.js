a44e35f149c92877b61648a70d4c1dfbc55a0ee4
rokasuman <roka7362@gmail.com>
2026-04-12 02:03:36 +1000

    email

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ import adminRouter from "./routes/adminRoute.js";
 import doctorRouter from "./routes/doctorRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import { sendAppointmentEmail } from "./utils/sendEmail.js"; 
-import dns from "dns";
-dns.setDefaultResultOrder("ipv4first");
+
 
 // app configuration 
 const app = express();
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,7 +4,6 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure:false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
